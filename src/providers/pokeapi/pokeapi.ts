e732843bb96223dcd2971c4bd498da86bca08769
Héctor Vargas <hector.vargas.infante@gmail.com>
2018-03-24 10:41:39 -0300

    pokeapi service connection

---
+++ src/providers/pokeapi/pokeapi.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the PokeapiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class PokeapiProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello PokeapiProvider Provider');
+  }
+
+  getPokemon(name) {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  }
+
+}
